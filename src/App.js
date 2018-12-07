4bf62d9032a3cfe50acd26069d2236f77f6f2ee9
Sasha <hammerx1@rambler.ru>
2018-12-07 13:37:53 +0300

    add first

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Car from './Car/Car';
 
 class App extends Component {
+
+    state = {
+        cars: [
+            {name: 'Ford', year: 2019},
+            {name: 'MacLaren', year: 2009},
+            {name: 'Fiat', year: 2090}
+        ],
+        pageTitle: 'I feel your changes'
+    }
+
+    changeTitleHandler = () => {
+      let oldState = this.state.pageTitle
+      let newState = oldState + '(Changed)'
+        this.setState({
+            pageTitle: newState
+        })
+
+    }
+
   render() {
+      const divStyle = {
+      'textAlign': 'center'
+      }
+      const cars = this.state.cars
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style = {divStyle}>
+          <h1>{this.state.pageTitle}</h1>
+        <button onClick={this.changeTitleHandler}>Change title</button>
+          <Car name={cars[0].name} year={cars[0].year} />
+          <Car name={cars[1].name} year={cars[1].year} />
+          <Car name={cars[2].name} year={cars[2].year} />
       </div>
-    );
+    )
   }
 }
 
