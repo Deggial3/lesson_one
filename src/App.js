2e6d1061544da7708fd6f2b18620d4a65b12ebba
Sasha <hammerx1@rambler.ru>
2018-12-08 11:00:40 +0300

    addMap

--- src/App.js
+++ src/App.js
@@ -13,27 +13,35 @@ class App extends Component {
         pageTitle: 'I feel your changes'
     }
 
-    changeTitleHandler = () => {
-      let oldState = this.state.pageTitle
-      let newState = oldState + '(Changed)'
-        this.setState({
-            pageTitle: newState
-        })
+    changeTitleHandler = (newTitle) => {
 
+        this.setState({
+            pageTitle: newTitle
+        })
     }
 
   render() {
       const divStyle = {
       'textAlign': 'center'
       }
-      const cars = this.state.cars
+      //const cars = this.state.cars
     return (
       <div style = {divStyle}>
           <h1>{this.state.pageTitle}</h1>
-        <button onClick={this.changeTitleHandler}>Change title</button>
-          <Car name={cars[0].name} year={cars[0].year} />
-          <Car name={cars[1].name} year={cars[1].year} />
-          <Car name={cars[2].name} year={cars[2].year} />
+        <button onClick={this.changeTitleHandler.bind(this, 'Changed!!!')}>Change title</button>
+          {this.state.cars.map((car, index)=> {
+              return(
+                  <Car
+                      key={index}
+                      name={car.name}
+                      year={car.year}
+                      onChangeTitle={() => this.changeTitleHandler(car.name)}
+                  />
+              )
+          })}
+          {/*<Car name={cars[0].name} year={cars[0].year} />*/}
+          {/*<Car name={cars[1].name} year={cars[1].year} />*/}
+          {/*<Car name={cars[2].name} year={cars[2].year} />*/}
       </div>
     )
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -4,5 +4,6 @@ export default props => (
     <div>
         <h2>Car name: {props.name}</h2>
         <p>Year: <strong>{props.year}</strong></p>
+        <button onClick={props.onChangeTitle}>Onclick!</button>
 
 </div>)
